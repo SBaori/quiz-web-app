d21abb302c37d0d0ecd73a693b6148d43354dd78
Sahil_Baori <sahilbaori2@gmail.com>
2022-11-18 12:21:42 +0530

    added signup/signin logic

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 const e = require("express");
 const bp = require("body-parser");
 const qzusr = require("./database/qz_usr");
-const connect = require("./database/qz_conn");
+const { connect } = require("./database/qz_conn");
 const cors = require("cors");
+const { auth } = require("./database/qz_auth");
 
 const app = e();
 
 app.use(bp.json());
 app.use(cors());
 
-app.post("/signup",function(req,res){
+app.post("/signup",async function(req,res){
     // qzusr.addUser(req.body.name,req.body.email,req.body.pwd);
+    const docs = await auth(req.body.name,req.body.email,req.body.pwd);
+   if(!docs.length)
+   {
+    console.log("added!");
+    qzusr.addUser(req.body.name,req.body.email,req.body.pwd);
+    res.sendStatus(200);
+   }
+   else
+   {
+    console.log("user present");
+    res.sendStatus(403);
+   }
     // res.
-    console.log(req.body.name);
 });
 
 connect().then(() => {
