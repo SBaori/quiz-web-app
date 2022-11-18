990042e4af768a6acbab29c124abef9d8b0d8416
Sahil_Baori <sahilbaori2@gmail.com>
2022-11-18 07:05:12 +0530

    added cors and jason parsing

--- server/server.js
+++ server/server.js
@@ -2,14 +2,17 @@ const e = require("express");
 const bp = require("body-parser");
 const qzusr = require("./database/qz_usr");
 const connect = require("./database/qz_conn");
+const cors = require("cors");
 
 const app = e();
 
-app.use(bp.urlencoded({extended: true}));
+app.use(bp.json());
+app.use(cors());
 
 app.post("/signup",function(req,res){
-    qzusr.addUser(req.body.name,req.body.email,req.body.pwd);
+    // qzusr.addUser(req.body.name,req.body.email,req.body.pwd);
     // res.
+    console.log(req.body.name);
 });
 
 connect().then(() => {
